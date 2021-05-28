c75abd0e70a71471144e29fb6fc70b419186c6ca
ArielCalisaya <calisayaariel62@gmail.com>
2021-05-28 12:55:16 -0400

    changed footer link

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ export default function Home({ quizList }) {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://breatheco.de/"
           target="_blank"
           rel="noopener noreferrer"
         >
